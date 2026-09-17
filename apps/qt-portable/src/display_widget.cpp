a7c8d4e86c7ff8547d3256fd71d9d87a7da37c1b
rtzor <73038556+rtzor@users.noreply.github.com>
2026-09-17 21:25:44 +0200

    qt: add selectable OpenGL and CRT presentation

--- apps/qt-portable/src/display_widget.cpp
+++ apps/qt-portable/src/display_widget.cpp
@@ -2,23 +2,73 @@
 #include "display_widget.h"
 
 #include <QKeyEvent>
+#include <QOpenGLWidget>
 #include <QPainter>
 #include <QPaintEvent>
+#include <QRadialGradient>
+#include <QStackedLayout>
 
 #include <algorithm>
+#include <cmath>
 
-DisplayWidget::DisplayWidget(QWidget *parent) : QWidget(parent)
+class SoftwareDisplayCanvas final : public QWidget {
+public:
+    explicit SoftwareDisplayCanvas(DisplayWidget *owner)
+        : QWidget(owner), owner_(owner)
+    {
+        setFocusPolicy(Qt::NoFocus);
+        setAttribute(Qt::WA_TransparentForMouseEvents);
+    }
+
+protected:
+    void paintEvent(QPaintEvent *event) override
+    {
+        (void) event;
+        QPainter painter(this);
+        owner_->paintPresentation(painter, rect());
+    }
+
+private:
+    DisplayWidget *owner_;
+};
+
+class OpenGLDisplayCanvas final : public QOpenGLWidget {
+public:
+    explicit OpenGLDisplayCanvas(DisplayWidget *owner)
+        : QOpenGLWidget(owner), owner_(owner)
+    {
+        setFocusPolicy(Qt::NoFocus);
+        setAttribute(Qt::WA_TransparentForMouseEvents);
+        setUpdateBehavior(QOpenGLWidget::NoPartialUpdate);
+    }
+
+protected:
+    void paintGL() override
+    {
+        QPainter painter(this);
+        owner_->paintPresentation(painter, rect());
+    }
+
+private:
+    DisplayWidget *owner_;
+};
+
+DisplayWidget::DisplayWidget(QWidget *parent)
+    : QWidget(parent), surfaces_(new QStackedLayout(this)),
+      canvas_(new SoftwareDisplayCanvas(this))
 {
     setFocusPolicy(Qt::StrongFocus);
     setMinimumSize(720, 400);
     setAutoFillBackground(false);
+    surfaces_->setContentsMargins(0, 0, 0, 0);
+    surfaces_->addWidget(canvas_);
 }
 
 void
 DisplayWidget::setFrame(const QImage &frame)
 {
     frame_ = frame;
-    update();
+    canvas_->update();
 }
 
 QImage
@@ -50,7 +100,7 @@ DisplayWidget::setScaleMode(ScaleMode mode)
     if (scaleMode_ == mode)
         return;
     scaleMode_ = mode;
-    update();
+    canvas_->update();
 }
 
 DisplayWidget::ScaleMode
@@ -65,13 +115,49 @@ DisplayWidget::setSmoothScaling(bool enabled)
     if (smoothScaling_ == enabled)
         return;
     smoothScaling_ = enabled;
-    update();
+    canvas_->update();
 }
 
 bool
 DisplayWidget::smoothScaling() const
 {
     return smoothScaling_;
+}
+
+void
+DisplayWidget::setRenderer(Renderer renderer)
+{
+    if (renderer_ == renderer)
+        return;
+    renderer_ = renderer;
+    surfaces_->removeWidget(canvas_);
+    delete canvas_;
+    canvas_ = renderer == Renderer::OpenGL ?
+        static_cast<QWidget *>(new OpenGLDisplayCanvas(this)) :
+        static_cast<QWidget *>(new SoftwareDisplayCanvas(this));
+    surfaces_->addWidget(canvas_);
+    canvas_->update();
+}
+
+DisplayWidget::Renderer
+DisplayWidget::renderer() const
+{
+    return renderer_;
+}
+
+void
+DisplayWidget::setEffect(Effect effect)
+{
+    if (effect_ == effect)
+        return;
+    effect_ = effect;
+    canvas_->update();
+}
+
+DisplayWidget::Effect
+DisplayWidget::effect() const
+{
+    return effect_;
 }
 
 void
@@ -113,15 +199,36 @@ DisplayWidget::targetRect(const QSize &frameSize, const QSize &viewportSize,
 }
 
 void
-DisplayWidget::paintEvent(QPaintEvent *event)
+DisplayWidget::paintPresentation(QPainter &painter, const QRect &viewport) const
 {
-    QPainter painter(this);
-    (void) event;
-    painter.fillRect(rect(), Qt::black);
+    painter.fillRect(viewport, Qt::black);
     if (frame_.isNull())
         return;
+    const QRect target = targetRect(frame_.size(), viewport.size(), scaleMode_);
     painter.setRenderHint(QPainter::SmoothPixmapTransform, smoothScaling_);
-    painter.drawImage(targetRect(frame_.size(), size(), scaleMode_), frame_);
+    painter.drawImage(target, frame_);
+    if (effect_ != Effect::Crt)
+        return;
+
+    painter.save();
+    painter.setClipRect(target);
+    const qreal sourceLineHeight = static_cast<qreal>(target.height()) /
+                                   static_cast<qreal>(frame_.height());
+    const int scanlineStep = std::max(2, qRound(sourceLineHeight * 2.0));
+    painter.setPen(QPen(QColor(0, 0, 0, 52), 1));
+    for (int y = target.top() + scanlineStep - 1; y <= target.bottom();
+         y += scanlineStep)
+        painter.drawLine(target.left(), y, target.right(), y);
+
+    const QPointF center = target.center();
+    const qreal radius = std::hypot(static_cast<qreal>(target.width()),
+                                    static_cast<qreal>(target.height())) * 0.52;
+    QRadialGradient vignette(center, radius);
+    vignette.setColorAt(0.0, QColor(0, 0, 0, 0));
+    vignette.setColorAt(0.72, QColor(0, 0, 0, 0));
+    vignette.setColorAt(1.0, QColor(0, 0, 0, 105));
+    painter.fillRect(target, vignette);
+    painter.restore();
 }
 
 void
--- apps/qt-portable/src/display_widget.h
+++ apps/qt-portable/src/display_widget.h
@@ -9,6 +9,10 @@
 #include <functional>
 
 class QKeyEvent;
+class QPainter;
+class QStackedLayout;
+class OpenGLDisplayCanvas;
+class SoftwareDisplayCanvas;
 
 class DisplayWidget final : public QWidget {
 public:
@@ -17,6 +21,14 @@ public:
         Integer,
         CorrectedFourThree,
         Stretch
+    };
+    enum class Renderer {
+        Software,
+        OpenGL
+    };
+    enum class Effect {
+        None,
+        Crt
     };
 
     using KeyHandler = std::function<void(QKeyEvent *, bool)>;
@@ -30,21 +42,31 @@ public:
     ScaleMode scaleMode() const;
     void setSmoothScaling(bool enabled);
     bool smoothScaling() const;
+    void setRenderer(Renderer renderer);
+    Renderer renderer() const;
+    void setEffect(Effect effect);
+    Effect effect() const;
     void setKeyHandler(KeyHandler handler);
 
     static QRect targetRect(const QSize &frameSize, const QSize &viewportSize,
                             ScaleMode mode);
 
 protected:
-    void paintEvent(QPaintEvent *event) override;
     void keyPressEvent(QKeyEvent *event) override;
     void keyReleaseEvent(QKeyEvent *event) override;
 
 private:
+    friend class OpenGLDisplayCanvas;
+    friend class SoftwareDisplayCanvas;
+    void paintPresentation(QPainter &painter, const QRect &viewport) const;
     QImage frame_;
     KeyHandler keyHandler_;
+    QStackedLayout *surfaces_;
+    QWidget *canvas_;
     ScaleMode scaleMode_ = ScaleMode::Fit;
     bool smoothScaling_ = false;
+    Renderer renderer_ = Renderer::Software;
+    Effect effect_ = Effect::None;
 };
 
 #endif
